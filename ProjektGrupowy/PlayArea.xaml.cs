adc93ac0882c8a41b140f7f9a1801d9fa5691308
Aleksandra Kuper <aleksandra.kuper@wp.pl>
2017-02-05 16:13:12 +0100

    dodanie jeden przejazd piłki

--- ProjektGrupowy/PlayArea.xaml.cs
+++ ProjektGrupowy/PlayArea.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -19,9 +20,38 @@ namespace ProjektGrupowy
     /// </summary>
     public partial class PlayArea : Window
     {
+        private Storyboard myStoryboard;
+        Random random = new Random();
+        
+
         public PlayArea()
         {
             InitializeComponent();
+            this.Loaded += ChallengePage_Loaded;
+        }
+
+        // Należy poczekać aż całe okno sie wgra w celu uzyskania parametrów okna gry
+        private void ChallengePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            double startPositionOfBall_X = areaOfGame.ActualWidth / 2 - ball.ActualWidth / 2;
+
+            BallAnimation(0, areaOfGame.ActualWidth, new PropertyPath(Canvas.LeftProperty));
+            BallAnimation(random.Next((int)areaOfGame.ActualHeight), random.Next((int)areaOfGame.ActualHeight), new PropertyPath(Canvas.TopProperty));
+        }
+        private void BallAnimation(double from, double to, PropertyPath property)
+        {
+            DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+            myDoubleAnimation.From = from;
+            myDoubleAnimation.To = to;
+            myDoubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(3));
+            myDoubleAnimation.AutoReverse = true;
+          //  myDoubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+
+            myStoryboard = new Storyboard();
+            myStoryboard.Children.Add(myDoubleAnimation);
+            Storyboard.SetTargetName(myDoubleAnimation, "ball");
+            Storyboard.SetTargetProperty(myDoubleAnimation, property);
+            myStoryboard.Begin(ball);
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
